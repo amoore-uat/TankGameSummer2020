d02d578899800d3dc9413ca88756ae76294d325f
Adam Moore <amoore@uat.edu>
2020-07-13 13:14:58 -0700

    UI Setup for title menu, options menu, and player ui

---
+++ Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // What happens when the player presses the Start Game button
+        Debug.Log("Start game");
+    }
+
+    public void OnClickOptions()
+    {
+        // What happens when the player presses the options button
+        Debug.Log("Options");
+    }
+
+    public void OnClickQuit()
+    {
+        // What happens when the player presses the quit button
+        Debug.Log("Quit game");
+        Application.Quit();
+    }
+}
