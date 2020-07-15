ab0067e33987e8757d6cd50ffc454832fc4fbcab
Adam Moore <amoore@uat.edu>
2020-07-15 13:32:59 -0700

    Lecture about SaveManager and playerprefs

---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveManager : Singleton<SaveManager>
+{
+    public float musicVolume = 1.0f;
+    public float fxVolume = 1.0f;
+
+    void Start()
+    {
+        Load();        
+    }
+    public void Save()
+    {
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        PlayerPrefs.SetFloat("fxVolume", fxVolume);
+        PlayerPrefs.Save();
+    }
+
+    public void Load()
+    {
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            musicVolume = PlayerPrefs.GetFloat("MusicVolume");
+        }
+        if (PlayerPrefs.HasKey("fxVolume"))
+        {
+            fxVolume = PlayerPrefs.GetFloat("fxVolume");
+        }
+        
+    }
+
+    void OnApplicationQuit()
+    {
+        Save();
+    }
+}
