8ed50dca43623b68153c018abdbac67a81eaac07
Adam Moore <amoore@uat.edu>
2020-07-20 13:26:44 -0700

    High Score tracking partially implemented. Split Screen camera partial

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    private const int HIGHSCORETABLESIZE = 3;
     public GameObject playerPrefab;
     public int demoNumber = 13;
     public List<PlayerSpawnPoint> playerSpawnPoints;
+    public List<ScoreData> scores = new List<ScoreData>();
 
     protected override void Awake()
     {
         base.Awake();
         playerSpawnPoints = new List<PlayerSpawnPoint>();
+        scores.Sort();
+        scores.Reverse();
+        // Limit the size of the high score table
+        scores = scores.GetRange(index: 0, count: HIGHSCORETABLESIZE);
     }
 
     public void SpawnPlayer()
